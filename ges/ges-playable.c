31f2809a76be63e0ab8c9c26cd6484068ca8419b
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-06-07 22:47:58 +0200

    Document ges-playable.

--- ges/ges-playable.c
+++ ges/ges-playable.c
@@ -9,6 +9,21 @@ ges_playable_default_init (GESPlayableInterface * iface)
   iface->make_playable = NULL;
 }
 
+/**
+ * ges_playable_make_player:
+ *
+ * @playable: A #GESPlayable
+ *
+ * Create a #GstPlayer for @playable. Refer to the documentation of
+ * #GstPlayer for more information.
+ *
+ * Note that as this function will remove @playable from any container
+ * it was in (such as a #GESTimeline). This means you can't both play
+ * it and its containing timeline, disposing of the #GstPlayer will
+ * put back @playable in its original container.
+ *
+ * Returns: (transfer full): The newly created #GstPlayer.
+ */
 GstPlayer *
 ges_playable_make_player (GESPlayable * playable)
 {
